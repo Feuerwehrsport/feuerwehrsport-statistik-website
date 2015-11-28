0c159b4d1d6ffbe605d5cec0621d0179cb0cfd64
Georg Limbach <glimbach@milabent.com>
2015-11-28 09:39:39 +0100

    Live-Daten-Kopie reparieren

--- etc/clone-live-data-to-devel.sh
+++ etc/clone-live-data-to-devel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DB="fws-statistik"
-DROP_SQL=$(echo "select 'drop table if exists \"' || tablename || '\" cascade;' rom pg_tables where schemaname = 'public';" | psql -t -h localhost -U $DB $DB)
+DROP_SQL=$(echo "select 'DROP TABLE IF EXISTS \"' || tablename || '\" CASCADE;' FROM pg_tables WHERE schemaname = 'public';" | psql -t -h localhost -U $DB $DB)
 echo $DROP_SQL | psql -t -h localhost -U "$DB" "$DB"
 ssh "$DB@warnow" -C pg_dump -c -U "$DB" "$DB" | psql -h localhost -U "$DB" "$DB"
