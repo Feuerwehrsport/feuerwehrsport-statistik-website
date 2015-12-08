8833349e8ce18f9cfdb831c0243051fb1c5dbeab
Georg Limbach <glimbach@milabent.com>
2015-12-08 13:09:40 +0100

    Schema für User anpassen

--- db/migrate/20150928060950_create_users.rb
+++ db/migrate/20150928060950_create_users.rb
@@ -3,7 +3,9 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email_address
-      t.string :ip_address
+      t.string :ip_address_hash
+      t.string :user_agent_hash
+      t.string :user_agent_meta
 
       t.timestamps null: false
     end
