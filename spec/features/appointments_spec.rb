fc97a291083a5fc5739c4d1795354d296900b906
Georg Limbach <glimbach@milabent.com>
2015-12-12 17:27:50 +0100

    Feature-Tests für Termine

--- spec/features/appointments_spec.rb
+++ spec/features/appointments_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
-describe "appointments", type: :feature, js: true do
-  it "works" do
+describe "appointments", type: :feature, js: true, driver: :webkit do
+  it "can add appointment and edit one" do
     api_sign_in
 
+    # add one
     visit appointments_path
     find("#add-appointment").click
     within('.fss-window') do
       expect(page).to have_content("Termin hinzufügen")
       fill_in "Name", with: "Wintertraining_xyz"
-      fill_in "Beschreibung", with: "Beschreibung"
+      fill_in "Beschreibung", with: "Beschreibung\n123"
       select('Charlottenthal', from: 'Ort')
       select('D-Cup', from: 'Typ')
       check("Feuerwehrstafette")
@@ -17,6 +18,44 @@ describe "appointments", type: :feature, js: true do
     end
     expect(page).to_not have_content("Bitte warten")
     expect(page).to have_content("Wintertraining_xyz")
-    expect(Appointment.where(name: "Wintertraining_xyz").count).to eq 1
+
+    appointment = Appointment.where(name: "Wintertraining_xyz")
+    expect(appointment.count).to eq 1
+    expect(appointment.first.attributes.symbolize_keys).to include(
+      name: "Wintertraining_xyz",
+      dated_at: Date.today,
+      description: "Beschreibung\n123",
+      disciplines: "fs",
+      event_id: 1,
+      place_id: 1,
+      published_at: nil,
+    )
+
+    # edit one
+    click_on("Wintertraining_xyz")
+    find('#edit-appointment').click
+    within('.fss-window') do
+      expect(find_field('Name').value).to eq "Wintertraining_xyz"
+      expect(find_field("Beschreibung").value).to eq "Beschreibung\n123"
+      fill_in "Name", with: "Wintertraining_opü"
+      fill_in "Beschreibung", with: "Beschreibung\n890"
+      select('Tribsees', from: 'Ort')
+      select('MV-Cup', from: 'Typ')
+      check("Hakenleitersteigen")
+      click_on("OK")
+    end
+    expect(page).to have_content("Der Fehlerbericht wurde gespeichert")
+
+
+    change_request_content = ChangeRequest.last.content
+    expect(change_request_content).to include key: "edit-appointment"
+    expect(change_request_content[:data][:appointment]).to include( 
+      name: "Wintertraining_opü",
+      dated_at: Date.today.to_s,
+      description: "Beschreibung\n890",
+      disciplines: "fs,hl",
+      event_id: "13",
+      place_id: "3",
+    )
   end
 end
