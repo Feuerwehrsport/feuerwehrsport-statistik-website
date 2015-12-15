035536c7d8836314df8970d454c8a69fd62b5e2c
Georg Limbach <glimbach@milabent.com>
2015-12-15 23:08:58 +0100

    Importierte Daten in Dev-Modus laden

--- etc/clone-live-data-to-devel.sh
+++ etc/clone-live-data-to-devel.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 DB="fws-statistik"
+HOST="warnow"
+
 DROP_SQL=$(echo "select 'DROP TABLE IF EXISTS \"' || tablename || '\" CASCADE;' FROM pg_tables WHERE schemaname = 'public';" | psql -t -h localhost -U $DB $DB)
 echo $DROP_SQL | psql -t -h localhost -U "$DB" "$DB"
-ssh "$DB@warnow" -C pg_dump -c -U "$DB" "$DB" | psql -h localhost -U "$DB" "$DB"
+ssh "$DB@$HOST" -C pg_dump -c -U "$DB" "$DB" | psql -h localhost -U "$DB" "$DB"
+
+
+rsync -axz --info=progress2 --exclude="tmp/" --partial "$DB@$HOST:/srv/$DB/shared/uploads/" ./public/uploads/
