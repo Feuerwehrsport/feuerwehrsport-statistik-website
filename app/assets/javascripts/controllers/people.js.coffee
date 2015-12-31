d5163c0a7c179164289a438244c5c286a9367ee3
Georg Limbach <glimbach@milabent.com>
2015-12-31 16:04:43 +0100

    Change-Requests für Person fertigstellen

--- app/assets/javascripts/controllers/people.js.coffee
+++ app/assets/javascripts/controllers/people.js.coffee
@@ -23,3 +23,78 @@ $ () ->
           Fss.ajaxReload 'POST', 'people', person: personData
         )
         .open()
+
+  $('#add-change-request').click () ->
+    personId = $(this).data('person-id')
+    Fss.checkLogin () ->
+      options = [
+        { value: 'wrong', display: 'Person ist falsch geschrieben'}
+        { value: 'nation', display: 'Person ist falscher Nation zugeordnet'}
+        { value: 'other', display: 'Etwas anderes'}
+      ]
+      FssWindow.build('Auswahl des Fehlers')
+      .add(new FssFormRowDescription('Bitte wählen Sie das Problem aus:'))
+      .add(new FssFormRowRadio('what', 'Was ist passiert?', null, options))
+      .on('submit', (data) ->
+        selected = data.what
+        if selected is 'wrong'
+          options = [
+            { value: 'together', display: 'Richtige Schreibweise auswählen (für Administrator <i>VIEL</i> einfacher)'}
+            { value: 'correction', display: 'Selbst korrekte Schreibweise hinzufügen'}
+          ]
+          FssWindow.build('Korrektur des Fehlers')
+          .add(new FssFormRowDescription('Bitte wählen Sie die Korrekturmethode aus:'))
+          .add(new FssFormRowRadio('what', 'Korrektur wählen', null, options))
+          .on('submit', (data) ->
+            selected = data.what
+            if selected is 'correction'
+              Fss.getResource 'people', personId, (person) ->
+                FssWindow.build('Namen korrigieren')
+                .add(new FssFormRowDescription('Bitte korrigieren Sie den Namen:'))
+                .add(new FssFormRowText('firstname', 'Vorname', person.first_name))
+                .add(new FssFormRowText('name', 'Nachname', person.last_name))
+                .on('submit', (data) ->
+                  data.id = personId
+                  Fss.changeRequest("person-correction", person: data)
+                )
+                .open()
+            else if selected is 'together'
+              Fss.getResources 'people', (people) ->
+                options = []
+                for person in people
+                  continue if person.id is personId
+                  options.push
+                    value: person.id
+                    display: "#{person.last_name}, #{person.first_name} (#{person.translated_gender})"
+
+                FssWindow.build('Namen korrigieren')
+                .add(new FssFormRowDescription('Bitte wählen Sie die korrekte Person aus:'))
+                .add(new FssFormRowSelect('person_id', 'Richtige Person', null, options))
+                .on('submit', (data) ->
+                  Fss.changeRequest("person-together", person_id: personId, correct_person_id: data.person_id)
+                )
+                .open()
+            )
+            .open()
+        else if selected is 'other'
+          FssWindow.build('Fehler beschreiben')
+          .add(new FssFormRowDescription('Bitte beschreiben Sie das Problem:'))
+          .add(new FssFormRowTextarea('description', 'Beschreibung', ''))
+          .on('submit', (data) ->
+            Fss.changeRequest("person-other", person_id: personId, description: data.description)
+          )
+          .open()
+        else if selected is 'nation'
+          Fss.getResources 'nations', (nations) ->
+            nationOptions = nations.map (nation) ->
+              { value: nation.id, display: nation.name }
+
+            FssWindow.build('Andere Nation')
+              .add(new FssFormRowDescription('Bitte wählen Sie die richtige Nation:'))
+              .add(new FssFormRowSelect('nation_id', 'Nation', null, nationOptions))
+              .on('submit', (data) ->
+                Fss.changeRequest("person-change-nation", person_id: personId, nation_id: data.nation_id)
+              )
+            .open()
+      )
+      .open()
--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -15,10 +15,15 @@ describe "people", type: :feature, js: true, driver: :webkit do
       select('Österreich', from: 'Nation')
       click_on("OK")
     end
-    expect(page).to have_content("00AABBCC")
-    expect(page).to have_content("Vorname")
 
+    Timeout.timeout(5) do
+      loop do
+        person = Person.where(last_name: "00AABBCC")
+        break if person.present?
+      end
+    end
     person = Person.where(last_name: "00AABBCC")
+
     expect(person.count).to eq 1
     expect(person.first.attributes.symbolize_keys).to include(
       last_name: "00AABBCC",
@@ -26,5 +31,102 @@ describe "people", type: :feature, js: true, driver: :webkit do
       gender: 0,
       nation_id: 2,
     )
+  end
+
+  context "add change request" do
+    it "can put people together" do
+      api_sign_in
+
+      # add one
+      visit person_path(id: 42)
+      find("#add-change-request").click
+      within('.fss-window') do
+        expect(page).to have_content("Auswahl des Fehlers")
+        choose("Person ist falsch geschrieben")
+        click_on("OK")
+        
+        expect(page).to have_content("Korrektur des Fehlers")
+        choose("Richtige Schreibweise auswählen (für Administrator VIEL einfacher)")
+        click_on("OK")
+        
+        expect(page).to have_content("Namen korrigieren")
+        select("Limbach, Georg (männlich)", from: "Richtige Person")
+        click_on("OK")
+      end
+      expect(page).to have_content("Der Fehlerbericht wurde gespeichert")
+
+      change_request_content = ChangeRequest.last.content
+      expect(change_request_content).to eq(key: "person-together", data: { person_id: "42", correct_person_id: "271" })
+    end
+
+    it "can correct person" do
+      api_sign_in
+
+      # add one
+      visit person_path(id: 42)
+      find("#add-change-request").click
+      within('.fss-window') do
+        expect(page).to have_content("Auswahl des Fehlers")
+        choose("Person ist falsch geschrieben")
+        click_on("OK")
+        
+        expect(page).to have_content("Korrektur des Fehlers")
+        choose("Selbst korrekte Schreibweise hinzufügen")
+        click_on("OK")
+        
+
+        expect(page).to have_content("Namen korrigieren")
+        fill_in "Vorname", with: "Vorname"
+        fill_in "Nachname", with: "00AABBCC"
+        click_on("OK")
+      end
+      expect(page).to have_content("Der Fehlerbericht wurde gespeichert")
+
+      change_request_content = ChangeRequest.last.content
+      expect(change_request_content).to eq(key: "person-correction", data: { person: { firstname: "Vorname", name: "00AABBCC", id: "42" } })
+    end
+
+    it "can correct nation" do
+      api_sign_in
+
+      # add one
+      visit person_path(id: 42)
+      find("#add-change-request").click
+      within('.fss-window') do
+        expect(page).to have_content("Auswahl des Fehlers")
+        choose("Person ist falscher Nation zugeordnet")
+        click_on("OK")
+        
+        expect(page).to have_content("Andere Nation")
+        select("Ungarn", from: "Nation")
+        click_on("OK")
+      end
+      expect(page).to have_content("Der Fehlerbericht wurde gespeichert")
+
+      change_request_content = ChangeRequest.last.content
+      expect(change_request_content).to eq(key: "person-change-nation", data: { person_id: "42", nation_id: "3" })
+    end
+
+    it "can add description" do
+      api_sign_in
+
+      # add one
+      visit person_path(id: 42)
+      find("#add-change-request").click
+      within('.fss-window') do
+        expect(page).to have_content("Auswahl des Fehlers")
+        choose("Etwas anderes")
+        click_on("OK")
+        
+        expect(page).to have_content("Fehler beschreiben")
+        fill_in("Beschreibung", with: "Beschreibung\n123")
+        click_on("OK")
+      end
+      expect(page).to have_content("Der Fehlerbericht wurde gespeichert")
+
+      change_request_content = ChangeRequest.last.content
+      expect(change_request_content).to eq(key: "person-other", data: { person_id: "42", description: "Beschreibung\r\n123" })
+    end
+
   end
 end
