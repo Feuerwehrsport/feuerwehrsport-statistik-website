bc3ead9cf194fc9d445c072bc0b118d09cc3311c
Georg Limbach <glimbach@milabent.com>
2016-01-12 13:40:07 +0100

    API: group_score_types #index erweitern

--- app/controllers/api/group_score_types_controller.rb
+++ app/controllers/api/group_score_types_controller.rb
@@ -1,5 +1,13 @@
 module API
   class GroupScoreTypesController < BaseController
     include CRUD::IndexAction
+
+    protected
+    
+    def base_collection
+      super_collection = super
+      super_collection = super_collection.where(discipline: params[:discipline]) if params[:discipline].present?
+      super_collection
+    end
   end
 end
--- spec/controllers/api/group_score_types_controller_spec.rb
+++ spec/controllers/api/group_score_types_controller_spec.rb
@@ -12,5 +12,19 @@ RSpec.describe API::GroupScoreTypesController, type: :controller do
         regular: true,
       )
     end
+
+    context "when discipline given" do
+      before { GroupScoreCategory.last.update!(group_score_type: GroupScoreType.where(discipline: :gs).first) }
+      it "returns group_score_categories" do
+        get :index, discipline: "gs"
+        expect_json_response
+        expect(json_body[:group_score_types].first).to eq(
+          discipline: "gs",
+          id: 2,
+          name: "WKO",
+          regular: true,
+        )
+      end
+    end
   end
 end
