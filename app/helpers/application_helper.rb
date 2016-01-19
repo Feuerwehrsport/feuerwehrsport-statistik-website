fcf25ecfeb7b0d4524d0c71e8a88e3eb10ef666c
Georg Limbach <glimbach@milabent.com>
2016-01-19 23:48:11 +0100

    Years schöner gestalten

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -137,7 +137,7 @@ module ApplicationHelper
 
   def score_links(scores)
     scores.map do |score|
-      link_to(score.second_time, score.competition, title: score.competition.event)
+      link_to(score.second_time, score.competition, title: score.competition)
     end.join(", ").html_safe
   end
 end
