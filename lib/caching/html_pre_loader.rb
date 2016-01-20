64466bcae7860bb073dd807a194d9ad2be5eef02
Georg Limbach <glimbach@milabent.com>
2016-01-20 22:22:36 +0100

    HTML-Preloader erweitern

--- lib/caching/html_pre_loader.rb
+++ lib/caching/html_pre_loader.rb
@@ -8,13 +8,16 @@ module Caching
     def perform
       if Rails.env.production?
         urls.each do |url|
-          open("#{Rails.configuration.base_url}#{url}").read rescue nil
+          open("#{Rails.configuration.base_url}#{url}", read_timeout: 25).read rescue nil
         end
       end
     end
 
     def urls
       urls = [
+        root_path,
+        appointments_path,
+        change_logs_path,
         competitions_path,
         people_path,
         places_path,
@@ -22,12 +25,15 @@ module Caching
         years_path,
         events_path,
         series_rounds_path,
+        best_of_path,
       ]
       urls += Competition.order(created_at: :desc).pluck(:id).map { |id| competition_path(id) }
       urls += Person.order(created_at: :desc).pluck(:id).map { |id| person_path(id) }
       urls += Place.order(created_at: :desc).pluck(:id).map { |id| place_path(id) }
       urls += Team.order(created_at: :desc).pluck(:id).map { |id| team_path(id) }
-      urls += Year.pluck(:year).map { |id| year_path(id) }
+      urls += Year.pluck(:year).map { |id| year_path(id.to_i) }
+      urls += Year.pluck(:year).map { |id| best_performance_year_path(id.to_i) }
+      urls += Year.pluck(:year).map { |id| best_scores_year_path(id.to_i) }
       urls += Event.order(created_at: :desc).pluck(:id).map { |id| event_path(id) }
       urls += Series::Round.order(created_at: :desc).pluck(:id).map { |id| series_round_path(id) }
       urls += Series::PersonAssessment.order(created_at: :desc).pluck(:id).map { |id| series_assessment_path(id) }
