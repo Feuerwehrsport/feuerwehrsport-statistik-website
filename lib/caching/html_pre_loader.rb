dca5423bf0af30eb6d5342bb534e568951980e29
Georg Limbach <glimbach@milabent.com>
2016-01-22 11:20:20 +0100

    HTML-Preloader auf wichtige Seiten beschränken

--- lib/caching/html_pre_loader.rb
+++ lib/caching/html_pre_loader.rb
@@ -27,16 +27,11 @@ module Caching
         series_rounds_path,
         best_of_path,
       ]
-      urls += Competition.order(created_at: :desc).pluck(:id).map { |id| competition_path(id) }
-      urls += Person.order(created_at: :desc).pluck(:id).map { |id| person_path(id) }
       urls += Place.order(created_at: :desc).pluck(:id).map { |id| place_path(id) }
-      urls += Team.order(created_at: :desc).pluck(:id).map { |id| team_path(id) }
       urls += Year.pluck(:year).map { |id| year_path(id.to_i) }
       urls += Year.pluck(:year).map { |id| best_performance_year_path(id.to_i) }
       urls += Year.pluck(:year).map { |id| best_scores_year_path(id.to_i) }
       urls += Event.order(created_at: :desc).pluck(:id).map { |id| event_path(id) }
-      urls += Series::Round.order(created_at: :desc).pluck(:id).map { |id| series_round_path(id) }
-      urls += Series::PersonAssessment.order(created_at: :desc).pluck(:id).map { |id| series_assessment_path(id) }
     end
   end
 end
