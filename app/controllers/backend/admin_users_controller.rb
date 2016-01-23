ad32b1d74a51090fc305fbd84544973beb701872
Georg Limbach <glimbach@milabent.com>
2016-01-23 12:28:50 +0100

    Backend: Form für AdminUser anpassen

--- app/controllers/backend/admin_users_controller.rb
+++ app/controllers/backend/admin_users_controller.rb
@@ -3,7 +3,7 @@ module Backend
     protected
 
     def permitted_attributes
-      super.permit(:name, :role, :email, :password, :password_confirmation)
+      super.permit(:name, :email, :role)
     end
   end
 end
