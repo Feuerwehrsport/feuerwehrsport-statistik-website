c130c2fa3bc863d12d4d96f5c3ba1be7953cea88
Georg Limbach <glimbach@milabent.com>
2016-01-28 13:02:17 +0100

    CRUD: Index-Action globaler gestalten

--- app/controllers/concerns/api/crud/index_action.rb
+++ app/controllers/concerns/api/crud/index_action.rb
@@ -4,23 +4,15 @@ module API
       extend ActiveSupport::Concern
       
       included do
-        include ::CRUD::ObjectAssignment
-        before_action :assign_collection_for_index, only: :index
+        include ::CRUD::IndexAction
+        include InstanceMethods
       end
 
-      def index
-        if resource_collection.respond_to?(:index_order)
-          self.resource_collection = resource_collection.index_order
+      module InstanceMethods
+
+        def index
+          success(resource_variable_name.pluralize.to_sym => resource_collection.decorate)
         end
-        self.resource_collection = resource_collection.accessible_by(current_ability)
-        success(resource_variable_name.pluralize.to_sym => resource_collection.decorate)
-      end
-
-      protected
-
-      def assign_collection_for_index
-        assign_collection
-        authorize!(action_name.to_sym, resource_class)
       end
     end
   end
