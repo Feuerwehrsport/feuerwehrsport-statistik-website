02316a75930fb9f7592dc5e7a4acaa0fa9822f5c
Georg Limbach <glimbach@milabent.com>
2016-01-28 11:43:39 +0100

    Tests für Übersetzung anpassen
    refs #5

--- spec/support/feature_helper.rb
+++ spec/support/feature_helper.rb
@@ -3,7 +3,7 @@ def sign_in
   visit backend_root_path
   fill_in 'E-Mail-Adresse', with: 'a@a.de'
   fill_in 'Passwort', with: 'asdf1234'
-  click_button 'Log in'
+  click_button 'Anmelden'
   expect(page).to have_content 'Erfolgreich angemeldet'
 end
 
