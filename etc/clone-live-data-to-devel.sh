a8003a00a642889b48e9cf1692e9cce9218558ed
Georg Limbach <glimbach@milabent.com>
2016-03-06 14:36:25 +0100

    Import-Skript anpassen

--- etc/clone-live-data-to-devel.sh
+++ etc/clone-live-data-to-devel.sh
@@ -7,4 +7,6 @@ echo $DROP_SQL | psql -t -h localhost -U "$DB" "$DB"
 ssh "$DB@$HOST" -C pg_dump -c -U "$DB" "$DB" | psql -h localhost -U "$DB" "$DB"
 
 
-rsync -axz --info=progress2 --exclude="tmp/" --partial "$DB@$HOST:/srv/$DB/shared/uploads/" ./public/uploads/
+rsync -axz --info=progress2 -v --exclude="tmp/" --exclude="wettkampf_manager/" --partial "$DB@$HOST:/srv/$DB/shared/uploads/" ./public/uploads/
+
+rails r "AdminUser.first.update_attributes(password: 'Admin123')"
