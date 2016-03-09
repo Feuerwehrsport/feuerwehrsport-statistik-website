8660c90b5ed9b4e7cbdadb67d67ecd117c1510ab
Georg Limbach <glimbach@milabent.com>
2016-03-09 10:11:20 +0100

    ChangeLog-E-Mails nicht an sich selber schicken

--- app/mailers/change_request_mailer.rb
+++ app/mailers/change_request_mailer.rb
@@ -1,7 +1,7 @@
 class ChangeRequestMailer < ApplicationMailer
   def new_notification(change_request)
     @change_request = change_request.decorate
-    to = AdminUser.change_request_notification_receiver.map(&:named_email_address)
+    to = AdminUser.change_request_notification_receiver.where.not(id: change_request.admin_user_id).map(&:named_email_address)
     mail(to: to, subject: 'Fehler bei Feuerwehrsport-Statistik')
   end
 end
