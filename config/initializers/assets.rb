6a6cee9922cc48be194591bb825dc0f55d6fd786
Georg Limbach <glimbach@milabent.com>
2016-03-29 09:19:36 +0200

    Assets-Precompile schneller machen

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,13 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-
-Rails.application.config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
-Rails.application.config.assets.precompile += %w( ckeditor/* )
-
+Rails.application.config.assets.precompile += %w( controllers/* application.* backend.* comp_reg.* leaflet.* )
 
 Ckeditor.setup do |config|
   config.assets_languages = ['de']
