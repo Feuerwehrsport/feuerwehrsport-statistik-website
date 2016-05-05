ea0c34001e4d90f32ddedfcd89fced0e3fc8f6f2
Georg Limbach <glimbach@milabent.com>
2016-05-05 11:20:34 +0200

    Fehlende Action ergänzen
    refs #1

--- app/controllers/change_logs_controller.rb
+++ app/controllers/change_logs_controller.rb
@@ -1,6 +1,10 @@
 class ChangeLogsController < ResourceController
   cache_actions :index
 
+  def show
+    redirect_to action: :index
+  end
+
   def index
     @change_logs = ChangeLog.free_access.order(created_at: :desc).decorate.first(1000)
   end
