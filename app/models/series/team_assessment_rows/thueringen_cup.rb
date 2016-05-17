fb1ed87ab47ad7bf0143061e929e7b5839f7ed22
Georg Limbach <glimbach@milabent.com>
2016-05-17 22:10:56 +0200

    Thüringen-Cup-Bewertung korrigieren

--- app/models/series/team_assessment_rows/thueringen_cup.rb
+++ app/models/series/team_assessment_rows/thueringen_cup.rb
@@ -4,45 +4,13 @@ module Series
       def <=> other
         compare = other.points <=> points
         return compare if compare != 0
-        compare = other.participation_count <=> participation_count
-        return compare if compare != 0
-        sum_time <=> other.sum_time
-      end
-
-      def calculate_rank!(other_rows)
-        current_rank = 0
-        other_rows.each do |rank_row|
-          if rank_row.participation_count < 3
-            return @rank = nil if rank_row == self
-            next
-          end
-          current_rank += 1
-          if 0 == (self <=> rank_row)
-            return @rank = current_rank
-          end
-        end
-      end
-
-      def honor_sort other
-        compare = other.points <=> points
-        return compare if compare != 0
-        compare = other.participation_count <=> participation_count
-        return compare if compare != 0
         compare = best_rank <=> other.best_rank
         return compare if compare != 0
         compare = other.best_rank_count <=> best_rank_count
         return compare if compare != 0
+        compare = other.ordered_participations.count <=> ordered_participations.count
+        return compare if compare != 0
         sum_time <=> other.sum_time
-      end
-
-      def participation_count
-        @cups.values.count
-      end
-
-      def self.special_sort!(rows)
-        honor_rows = rows.select { |row| row.rank.present? && row.rank <= 3 }.sort { |row, other| row.honor_sort(other) }
-        honor_rows.each { |row| row.calculate_rank!(honor_rows) }
-        rows.sort_by! { |row| row.rank || 999 }
       end
 
       protected
@@ -57,7 +25,7 @@ module Series
 
       def sum_time
         @sum_time ||= begin
-          sum = @cups.values.flatten.map(&:time).sum
+          sum = ordered_participations.map(&:time).sum
           if sum >= TimeInvalid::INVALID
             TimeInvalid::INVALID
           else
