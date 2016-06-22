fd38165aa4a27f1b9b60bf852582cfcae55a9ee4
Georg Limbach <glimbach@milabent.com>
2016-06-22 15:16:47 +0200

    MV-Cup Import korrigieren

--- app/models/import/series/mv_single_cup.rb
+++ app/models/import/series/mv_single_cup.rb
@@ -12,7 +12,7 @@ module Import
 
       def configure_assessments
         [:female, :male].each do |gender|
-          person_ids = competition.scores.gender(gender).discipline(assessment_disciplines.keys.first).pluck(:person_id).uniq
+          person_ids = competition.scores.gender(gender).discipline(assessment_disciplines[:person].keys.first).pluck(:person_id).uniq
           people = Person.where(id: person_ids).order(:last_name, :first_name).decorate.map do |person|
             [person.id.to_s, person.full_name, true]
           end
