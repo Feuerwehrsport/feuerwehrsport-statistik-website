c9d0b27296ced26cc78b3843d3f67597505ab86e
Georg Limbach <georg.limbach@lichtbit.com>
2017-03-15 13:18:33 +0100

    Modulschreibweise korrigieren

--- app/controllers/series/rounds_controller.rb
+++ app/controllers/series/rounds_controller.rb
@@ -4,12 +4,12 @@ class Series::RoundsController < ResourceController
   def index
     @rounds = {}
     Series::Round.pluck(:name).uniq.sort.each do |name|
-      @rounds[name] = Round.cup_count.where(name: name)
+      @rounds[name] = Series::Round.cup_count.where(name: name)
     end
   end
 
   def show
-    round = Round.find(params[:id])
+    round = Series::Round.find(params[:id])
     @person_assessments = PersonAssessment.where(round: round).decorate
     @team_assessments_exists = TeamAssessment.where(round: round).present?
     @round = round.decorate
