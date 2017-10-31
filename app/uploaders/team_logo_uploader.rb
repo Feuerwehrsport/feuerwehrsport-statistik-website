4387a383d1bf077d1bc1d607f47d7a94dd3dc072
Georg Limbach <georg.limbach@lichtbit.com>
2017-10-31 14:31:06 +0100

    Team-Logo-Uploader optimieren

--- app/uploaders/team_logo_uploader.rb
+++ app/uploaders/team_logo_uploader.rb
@@ -4,13 +4,16 @@ class TeamLogoUploader < M3::ApplicationUploader
   end
 
   process resize_to_limit: [1000, 1000]
+  process_optimize_images
 
   version :tile do
     process resize_and_pad: [100, 100]
+    process_optimize_images
   end
 
   version :thumb do
     process resize_and_pad: [24, 24]
+    process_optimize_images
   end
 
   def extension_white_list
