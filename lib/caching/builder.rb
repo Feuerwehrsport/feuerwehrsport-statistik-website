63ff0285137eddae21030ab5bbe369b23c045ed3
Georg Limbach <georg.limbach@lichtbit.com>
2017-11-05 09:07:59 +0100

    Cache-Builder: weiter nach hinten verzögern

--- lib/caching/builder.rb
+++ lib/caching/builder.rb
@@ -2,15 +2,9 @@ class Caching::Builder
   include M3::Delayable
 
   def perform
-    remove_future_builders
+    return if same_future_jobs.present?
     Person.update_score_count
     Competition.update_discipline_score_count
     Caching::HTMLPreLoader.perform_now
-  end
-
-  private
-
-  def remove_future_builders
-    same_future_jobs.each(&:delete)
   end
 end
