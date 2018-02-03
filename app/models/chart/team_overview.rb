0ca4f9241d86eb157659b20f2478b64a9e0a3061
Georg Limbach <georg.limbach@lichtbit.com>
2018-02-03 10:58:00 +0100

    Aktive Mannschaften pro Jahr als Diagramm anzeigen

--- app/models/chart/team_overview.rb
+++ app/models/chart/team_overview.rb
@@ -10,6 +10,25 @@ module Chart
       states_pie(data)
     end
 
+    def activities_for_years
+      data = Year.where(Year.arel_table[:year].gt(1995)).pluck(:year).map do |year|
+        {
+          name: year.to_i.to_s,
+          y: TeamCompetition.joins(:competition)
+                            .where("EXTRACT(YEAR FROM competitions.date) = '#{year.to_i}'")
+                            .group(:team_id).count.count,
+        }
+      end
+      hc = lazy_high_chart
+      hc.legend(enabled: false)
+      hc.chart(type: 'column', height: 120)
+      hc.plotOptions(series: { pointWidth: 6 })
+      hc.yAxis(endOnTick: false, title: nil)
+      hc.xAxis(categories: data.map { |a| a[:name] })
+      hc.series(name: 'Mannschaften', data: data)
+      render(hc)
+    end
+
     protected
 
     def states_pie(data)
