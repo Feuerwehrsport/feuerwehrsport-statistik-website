767747198b7a6dd7d8c1f67d0b9cf22efed1c706
Georg Limbach <georg.limbach@lichtbit.com>
2019-05-02 22:02:35 +0200

    Wettkampfübersicht: Verbesserte Ansicht

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -5,15 +5,15 @@ class CompetitionsController < ResourceController
   datatable(:index, :competitions, Competition, collection: -> { Competition.includes(:place, :event) }) do |t|
     t.col(:date_iso, class: 'col-10', sortable: :date, default_order: :desc)
     t.col(:linked_name, class: 'col-40 info-link', searchable: [{ event: :name }, { place: :name }, :name], sortable: false)
-    t.col(:hb_female, searchable: false, th_class: 'small col-5')
-    t.col(:hb_male, searchable: false, th_class: 'small col-5')
-    t.col(:hl_female, searchable: false, th_class: 'small col-5')
-    t.col(:hb_male, searchable: false, th_class: 'small col-5')
-    t.col(:gs, searchable: false, th_class: 'small col-5')
-    t.col(:fs_female, searchable: false, th_class: 'small col-5')
-    t.col(:fs_male, searchable: false, th_class: 'small col-5')
-    t.col(:la_female, searchable: false, th_class: 'small col-5')
-    t.col(:la_male, searchable: false, th_class: 'small col-5')
+    t.col(:hb_female, searchable: false, class: 'hidden-xs', th_class: 'small col-5 hidden-xs')
+    t.col(:hb_male, searchable: false, class: 'hidden-xs', th_class: 'small col-5 hidden-xs')
+    t.col(:hl_female, searchable: false, class: 'hidden-xs', th_class: 'small col-5 hidden-xs')
+    t.col(:hb_male, searchable: false, class: 'hidden-xs', th_class: 'small col-5 hidden-xs')
+    t.col(:gs, searchable: false, class: 'hidden-xs', th_class: 'small col-5 hidden-xs')
+    t.col(:fs_female, searchable: false, class: 'hidden-xs', th_class: 'small col-5 hidden-xs')
+    t.col(:fs_male, searchable: false, class: 'hidden-xs', th_class: 'small col-5 hidden-xs')
+    t.col(:la_female, searchable: false, class: 'hidden-xs', th_class: 'small col-5 hidden-xs')
+    t.col(:la_male, searchable: false, class: 'hidden-xs', th_class: 'small col-5 hidden-xs')
   end
 
   def index
