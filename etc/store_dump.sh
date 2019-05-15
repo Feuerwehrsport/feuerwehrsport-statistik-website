da8a810c8d9e01d0ab208f0bd7a3e25638f878ad
Georg Limbach <georg.limbach@lichtbit.com>
2019-05-15 09:15:17 +0200

    pgtricks für Datenbank-Backups benutzen

--- etc/store_dump.sh
+++ etc/store_dump.sh
@@ -4,5 +4,7 @@ set -eu
 
 cd ~/feuerwehrsport-statistik-website-dump
 pg_dump $1 "$2" > dump.sql
+~/pgtricks/pgtricks/pg_dump_splitsort.py dump.sql
+rm dump.sql
 git commit -am "Backup $(date +'%Y-%m-%d')" > /dev/null
 git push > /dev/null 2>&1
