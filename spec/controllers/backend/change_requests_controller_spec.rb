ae0345e71b922fa9e29ec5b2a818f55b515060f7
Georg Limbach <georg.limbach@lichtbit.com>
2019-06-19 14:16:30 +0200

    ChangeRequestsController darf nur noch vom SubAdmin aufgerufen werden

--- spec/controllers/backend/change_requests_controller_spec.rb
+++ spec/controllers/backend/change_requests_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Backend::ChangeRequestsController, type: :controller do
   describe 'GET index' do
-    it 'successes' do
+    it 'successes', login: :sub_admin do
       get :index
       expect(response).to be_success
     end
