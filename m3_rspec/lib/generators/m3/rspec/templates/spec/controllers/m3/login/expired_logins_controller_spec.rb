aa7ae2ea41d545b62c0958030da6c5e777f66837
Georg Limbach <georg.limbach@lichtbit.com>
2021-09-28 11:00:54 +0200

    Unnötige Templates löschen

--- m3_rspec/lib/generators/m3/rspec/templates/spec/controllers/m3/login/expired_logins_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe M3::Login::ExpiredLoginsController, type: :controller, website: :default do
-  include_examples 'works like a expired logins controller'
-end
--- m3_rspec/lib/generators/m3/rspec/templates/spec/controllers/m3/login/password_resets_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe M3::Login::PasswordResetsController, type: :controller, website: :default do
-  include_examples 'works like a password resets controller'
-end
