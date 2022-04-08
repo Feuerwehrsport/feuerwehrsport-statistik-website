308af06fa5f96c016f29c79fa30484d990ca7dad
Georg Limbach <georg.limbach@lichtbit.com>
2022-04-08 12:57:09 +0200

    deploying-Git umstellen

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '~> 3.12.0'
 
 set :application, 'feuerwehrsport-statistik'
-set :repo_url, 'git@gitlab.lichtbit.com:feuerwehrsport/feuerwehrsport-statistik.git'
+set :repo_url, 'git@github.com:Feuerwehrsport/feuerwehrsport-statistik-website.git'
 set :deploy_to, '/srv/feuerwehrsport-statistik'
 
 set :rvm_ruby_version, '2.6.6'
