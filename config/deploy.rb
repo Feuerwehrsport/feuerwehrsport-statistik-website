ca50a9e7ba4cd1e207d49487e291d03f15ed2b32
Georg Limbach <georg.limbach@lichtbit.com>
2022-04-29 09:10:31 +0200

    Ruby und Gem-Update

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,6 @@ set :application, 'feuerwehrsport-statistik'
 set :repo_url, 'git@github.com:Feuerwehrsport/feuerwehrsport-statistik-website.git'
 set :deploy_to, '/srv/feuerwehrsport-statistik'
 
-set :rvm_ruby_version, '2.6.6'
+set :rvm_ruby_version, '2.7.6'
 
 set :systemd_usage, true
