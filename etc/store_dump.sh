0e504d47345d2e72bc876ce1379b57b93ecf0aeb
Georg Limbach <georg.limbach@lichtbit.com>
2023-06-14 09:17:11 +0200

    Dump-Erstellung korrigieren

--- etc/store_dump.sh
+++ etc/store_dump.sh
@@ -2,9 +2,10 @@
 
 set -eu
 
-cd ~/feuerwehrsport-statistik-website-dump
+cd /srv/feuerwehrsport-statistik/shared/dump-backup
 pg_dump $1 "$2" > dump.sql
-~/pgtricks/pgtricks/pg_dump_splitsort.py dump.sql
+/home/feuerwehrsport-statistik/.local/bin/pg_dump_splitsort dump.sql
 rm dump.sql
+git add . > /dev/null
 git commit -am "Backup $(date +'%Y-%m-%d')" > /dev/null
-git push > /dev/null 2>&1
+git push -q
