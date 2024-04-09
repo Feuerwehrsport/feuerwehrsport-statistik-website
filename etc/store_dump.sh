71dcddd39d99459faa28105f72b773ab1e94c0bf
Georg Limbach <georg.limbach@lichtbit.com>
2024-04-09 09:34:38 +0200

    virtual python env

--- etc/store_dump.sh
+++ etc/store_dump.sh
@@ -4,7 +4,7 @@ set -eu
 
 cd /srv/feuerwehrsport-statistik/shared/dump-backup
 pg_dump $1 "$2" > dump.sql
-/home/feuerwehrsport-statistik/.local/bin/pg_dump_splitsort dump.sql
+/home/feuerwehrsport-statistik/bin/pg_dump_splitsort dump.sql
 rm dump.sql
 git add . > /dev/null
 git commit -am "Backup $(date +'%Y-%m-%d')" > /dev/null
