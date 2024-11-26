dd7b1c2356b6780a0f008a1bb0c233d5bd8c08a8
Georg Limbach <georg.limbach@lichtbit.com>
2024-11-26 23:01:39 +0100

    deploy-Lock entfernen

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-# config valid for current version and patch releases of Capistrano
-lock '~> 3.17.2'
-
 set :application, 'feuerwehrsport-statistik'
 set :repo_url, 'git@github.com:Feuerwehrsport/feuerwehrsport-statistik-website.git'
 set :deploy_to, '/srv/feuerwehrsport-statistik'
